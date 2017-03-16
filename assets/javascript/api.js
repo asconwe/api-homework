7108bc28b21af23902cbe248550e21c87cf2a062
August Conwell <august.conwell@gmail.com>
2017-03-16 07:51:13 -0400

    Created buttons and ajax calls on clicks

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,32 +1,64 @@
 // When the document is ready
-$.ready(function(){
-    
-    // API endpoint URL
-    var apiBase = '';
-    // API Parameters
-    var apiSettings = '';
-    // The user's query
-    var userQuery = '';
-    // API key
-    var apiKey = '';
-    
-    // combine all variable into one Query URL
-    function queryURL() {
-        return apiBase + apiSettings + userQuery + apiKey;
+$( document ).ready(function(){
+    // Array of topics for buttons
+    var topics = ['Star Wars','Indiana Jones','Air Force One','Blade Runner'];
+
+    // Display the buttons
+    function displayButtons() {   
+        $('#displayButtons').empty(); 
+        topics.forEach(function(value){
+            arrValue = value.split(' ');
+            urlValue = arrValue.join('+');
+            $('#displayButtons').append('<button class="categoryButton" data-value="' + urlValue + '">' + value + '</button>');
+        });
+        setClickHandler();
     };
 
-    // On the click of a button
-    $('button').click(function(){
-        // Button that was clicked, saved as a variable
-        var button = $('this');
-        
-        userQuery = button.data('value')
-        
-        $.ajax({
-            url: queryURL(),
-            method: 'GET'
-        }).done(function(response){
-            
-        });
+    // API endpoint URL
+    var apiBase = 'http://api.giphy.com/v1/gifs/search?q=';
+    // The user's query
+    var userQuery = 'star wars';
+    // API Parameters
+    var apiParams = '&limit=10';
+    // API key
+    var apiKey = '&api_key=dc6zaTOxFJmzC';
+    
+    // combine all variable into one Query URL
+    function getQueryURL() {
+        return apiBase + userQuery  + apiParams + apiKey;
+    };
+    
+
+    $('#submit').click(function(){
+        var newTopic = $('#newTopic').val();
+        topics.push(newTopic);
+        displayButtons();
     });
+
+    // On the click of a button
+    function setClickHandler() {
+        $('.categoryButton').click(function(){
+            // Button that was clicked, saved as a variable
+            var button = $(this);
+            userQuery = button.data('value');
+            queryURL = getQueryURL();
+            $.ajax({
+                url: queryURL,
+                method: 'GET'
+            }).done(function(response){
+                $('#displayGifs').empty();
+                console.log(response);
+                var still;
+                var animate;
+                response.data.forEach(function(gif){
+                    $('#displayGifs').append('<img class="gif" data-state="still" src="' + gif.images.fixed_height_still.url + '"/>')
+                })
+                $('.gif').click(function(){
+                    
+                });
+            });
+        });
+    };
+    // Display the buttons on page-load
+    displayButtons();
 });
